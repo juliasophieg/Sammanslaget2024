b78565cc2855ace0de0b8c526e5f4fb18e558a38
Julia <jsgoransson@gmail.com>
2024-08-20 12:09:09 +0200

    added map

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const position = [57.7067, 11.9373]; //Koordinater
+
+  const locations = [
+    {
+      position: [57.7067, 11.9373],
+      title: "Berättelse av Anna",
+      story: "Lorem ipsum osv osv",
+    },
+    {
+      position: [57.71, 11.94],
+      title: "Berättelse av Johan",
+      story: "Lorem ipsum osv osv",
+    },
+    {
+      position: [57.709, 11.94],
+      title: "Berättelse av Maria",
+      story: "Lorem ipsum osv osv",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <h1>Map testing</h1>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {locations.map((location, index) => (
+        <Marker key={index} position={location.position}>
+          <Popup>
+            <h3>{location.title}</h3>
+            <p>{location.story}</p>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
 
-export default App
+export default App;
