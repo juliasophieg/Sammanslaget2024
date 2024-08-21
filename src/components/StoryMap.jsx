7451b8c02e6febf2c874e47ede114bf98c8d6611
hugosandsjo <hugosandsjo@gmail.com>
2024-08-21 19:51:16 +0200

    got supabase connection working

--- src/components/StoryMap.jsx
+++ src/components/StoryMap.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import StoryFooter from "./StoryFooter";
-import { storyArray } from "../../data/stories";
+import { storyArray } from "../../data/supabase";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useMap } from "../hooks/getLocation";
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://cpkibyqcwbytkhjcpowm.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNwa2lieXFjd2J5dGtoamNwb3dtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQyNTg5NTcsImV4cCI6MjAzOTgzNDk1N30.4oxOYUovu-sUGHUcbv_yEJ8LNcvukj-8hGaqLMV2D74"
+);
 
 export default function StoryMap() {
   const { position } = useMap();
@@ -15,9 +22,17 @@ export default function StoryMap() {
     author: "",
     age: "",
   });
-  const [stories, setStories] = useState(storyArray);
-
+  const [stories, setStories] = useState([]);
   console.log("stories:", stories);
+
+  useEffect(() => {
+    getStories();
+  }, []);
+
+  async function getStories() {
+    const { data } = await supabase.from("stories").select();
+    setStories(data);
+  }
 
   const currentLocation = new L.Icon({
     iconUrl: "/currentlocation.png",
@@ -53,7 +68,21 @@ export default function StoryMap() {
           </Popup>
         </Marker>
 
-        {stories.map((location, index) => (
+        {stories.map((story) => (
+          <Marker key={story.id} position={story.position}>
+            <Popup>
+              <h1>{story.title}</h1>
+              <p>{story.story}</p>
+              <p>{story.category}</p>
+              <div style={{ display: "flex", gap: "3px" }}>
+                <p>{story.author}</p>
+                <p>{story.age}</p>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+
+        {/* {stories.map((location, index) => (
           <Marker key={index} position={location.position}>
             <Popup>
               <h3>{location.title}</h3>
@@ -65,7 +94,7 @@ export default function StoryMap() {
               </div>
             </Popup>
           </Marker>
-        ))}
+        ))} */}
       </MapContainer>
 
       {/* Form at the bottom of the viewport */}
